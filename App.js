45db49bc394d16f623dff52d982fd43c0e31f993
lorislolo <lorena.macedo@aluno.ifsp.edu.br>
2024-02-15 17:37:19 -0300

    estilizando para a Hello Kitty

--- App.js
+++ App.js
@@ -1,22 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
     <View style={styles.header}>
-      <View style={styles.bolinha}></View>
-      <Text style={styles.texto}>Lorena Marques</Text>
-      <StatusBar style="auto" />
+        <View style={styles.bolinha}>
+          <Image source={require('./assets/helloKitty.jpeg')} style={styles.imageBackground}></Image>
+        </View>
+        <Text style={styles.texto}>Hello Kitty Cozinheira</Text>
+        <StatusBar style="auto" />
 
-      
-    </View>
+        
+      </View>
     <View style={styles.menu}>
-     <Text style={styles.texto}> Ocean Ocean </Text>
+     <Text style={styles.titulo}> Receita de bolo da Kitty </Text>
+     
      <StatusBar style="auto" />
     </View>
     <View style={styles.rodape}>
-     <Text style={styles.texto}>Copy 2024 </Text>
+     <Text style={styles.texto}> Kitty Copy 2024 </Text>
      <StatusBar style="auto" />
      </View>
     </View>
@@ -31,10 +34,15 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     margin: 0,
   },
+  imageBackground: {
+    width: 70,
+    height: 70,
+    borderRadius: 50,
+  },
   header: {
     flex: 1,
     width: '100%',
-    backgroundColor: '#24b5c6',
+    backgroundColor: '#faacc4',
     alignItems: 'center',
     flexDirection: 'row',
   },
@@ -46,19 +54,24 @@ const styles = StyleSheet.create({
     marginHorizontal: 10,
   },
   texto:{
-    color: '#E6E8FA',
+    color: '#8c2641',
     fontSize: 30,
+  },
+  titulo:{
+    fontSize: 40,
+    color: '#f32634',
+    fontWeight: 10,
   },
   menu: {
     flex: 4,
-    backgroundColor: '#CCFFFF',
+    backgroundColor: '#f2ced8',
     width: '100%',
     alignItems: 'center',
     justifyContent: 'center',
   },
    rodape: {
     flex: 1,
-    backgroundColor: '#24b5c6',
+    backgroundColor: '#faacc4',
     alignItems: 'center',
     width: '100%',
     justifyContent: 'center',
